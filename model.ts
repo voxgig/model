34eea8da6d5d2a7c7d1f68541680b289935c4ad1
rjrodger <richard@ricebridge.com>
2021-04-20 22:02:20 +0100

    v0.0.4

--- model.ts
+++ model.ts
@@ -1,11 +1,12 @@
 
 import { Jsonic } from 'jsonic'
 import { Dynamic } from 'jsonic/plugin/dynamic'
+import { Multifile } from 'jsonic/plugin/multifile'
 
 
 class Model {
   root: any = {}
-  parse = Jsonic.make().use(Dynamic)
+  parse = Jsonic.make().use(Dynamic).use(Multifile)
 
   constructor(spec: any) {
     this.root = this.parse(spec.src, { fileName: spec.path })
